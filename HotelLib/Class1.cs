0e7d5035b534a5fd80f447689204bccea3887069
h672645 <672645@stud.hvl.no>
2024-04-14 17:24:33 +0200

    Add project files.

---
+++ HotelLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace HotelLib
+{
+    public class HotelRoom
+    {
+        private int Id {  get; set; }
+
+        private int NumberOfBeds {  get; set; }
+
+        private int SizeOfRoom { get; set; }
+
+        private string QualityOfRoom { get; set; }
+
+        public HotelRoom(int  NumberOfBeds, int SizeOfRoom, string QualityOfRoom)
+        {
+            this.NumberOfBeds = NumberOfBeds;
+            this.SizeOfRoom = SizeOfRoom;
+            this.QualityOfRoom = QualityOfRoom;
+        }
+    }
+}
