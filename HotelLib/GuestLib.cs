fc12adf7bd82c99f76f6ada5fe3ec8136fef07a0
h672645 <672645@stud.hvl.no>
2024-04-14 19:35:30 +0200

    Oppdatert

--- HotelLib/GuestLib.cs
+++ HotelLib/GuestLib.cs
@@ -1,14 +1,44 @@
 ﻿
 
-namespace GuestLib
+namespace UserLib
 {
-    public class Guest
+    public class Person
     {
-        private string Navn { get; set; }  
-        private string Etternavn { get; set; }  
-        public Guest(string Navn, string Etternavn) {
+        public string Navn { get; set; }
+
+        public string Etternavn { get; set; }
+
+        public Person(string navn, string etternavn)
+        {
+            this.Navn = navn;
+            this.Etternavn = etternavn;
+        }
+
+    }
+
+    public class Guest : Person
+    {
+        public Guest(string Navn, string Etternavn) : base(Navn, Etternavn) {
             this.Navn = Navn;
             this.Etternavn = Etternavn;
+        }
+    }
+
+    public class Employee : Person
+    {
+        private String Stilling {  get; set; }
+        public Employee(string Navn, string Etternavn, string Stilling) : base(Navn, Etternavn)
+        {
+            this.Stilling = Stilling;
+        }
+
+    }
+
+    public class Admin : Person
+    {
+        public Admin(string Navn, String etternavn) : base(Navn, etternavn) 
+        {
+            
         }
     }
 }
---
+++ HotelLib/HotelLib.cs
@@ -0,0 +1,79 @@
+﻿using UserLib;
+
+namespace HotelLib
+{
+    public class Hotel
+    {
+        private string Navn {  get; set; }
+
+        private string HotellKjede { get; set; }
+
+        private string Beskrivelse { get; set; }
+
+
+
+        private HotelRoom[] HotelRooms { get; set; }
+
+        private HotelReservation[] HotelReservations { get; set; }
+
+
+
+    }
+    public class HotelRoom
+    {
+        private int Id {  get; set; }
+
+        private int NumberOfBeds {  get; set; }
+
+        private int SizeOfRoom { get; set; }
+
+        private string QualityOfRoom { get; set; }
+
+        public bool Occupied { get; set; }
+
+        public HotelRoom(int  NumberOfBeds, int SizeOfRoom, string QualityOfRoom)
+        {
+            this.NumberOfBeds = NumberOfBeds;
+            this.SizeOfRoom = SizeOfRoom;
+            this.QualityOfRoom = QualityOfRoom;
+            this.Occupied = false;
+        }
+    }
+
+    public class HotelReservation
+    {
+        private int Id {  get; set; }
+        private Guest Guest { get; set; }
+
+        private DateTime StartDate { get; set; }
+
+        private DateTime EndDate { get; set; }
+        private HotelRoom HotelRoom { get; set; }
+
+        public HotelReservation(Guest guest, HotelRoom hotelRoom, DateTime StartDate, DateTime EndDate, DateTime currentTime)
+        {
+            this.Guest = guest;
+            this.HotelRoom = hotelRoom;
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+            IsReservationActive(currentTime);
+        }
+
+        public bool IsReservationActive(DateTime currentDate)
+        {
+            if (HotelRoom == null)
+            {
+                return false;
+            }
+
+            if (currentDate >= StartDate && currentDate < EndDate)
+            {
+                HotelRoom.Occupied = true;
+                return true;
+            }
+
+            HotelRoom.Occupied = false;
+            return false;
+        }
+    } 
+}
