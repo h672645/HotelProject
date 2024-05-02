e2226bb0b13248a8ba19e80dcf3983d89a17e1f2
L672652 <672652@stud.hvl.no>
2024-05-02 12:31:47 +0200

    Prøving

---
+++ HotelWebsite/Pages/CRUD/BestiltRom.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HotelWebsite.NET.YourOutputDirectory;
+
+namespace HotelWebsite.NET.Pages.CRUD
+{
+    public class BestiltRomModel : PageModel
+
+    {
+
+        public int roomId { get; set; }
+        private readonly HotelWebsite.NET.YourOutputDirectory.MyFirstDatabaseContext _context;
+
+        public BestiltRomModel(HotelWebsite.NET.YourOutputDirectory.MyFirstDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            roomId = (int)TempData["RoomId"];
+            return Page();
+        }
+
+        [BindProperty]
+        public HotelRoom HotelRoom { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.HotelRooms.Add(HotelRoom);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ HotelWebsite/Pages/CRUD/FinnRom.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HotelWebsite.NET.YourOutputDirectory;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HotelWebsite.NET.Pages.CRUD
+{
+    public class FinnRomModel : PageModel
+    {
+        private readonly HotelWebsite.NET.YourOutputDirectory.MyFirstDatabaseContext _context;
+
+        public FinnRomModel(HotelWebsite.NET.YourOutputDirectory.MyFirstDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["GuestId"] = new SelectList(_context.Guests, "GuestId", "GuestId");
+        ViewData["RoomId"] = new SelectList(_context.HotelRooms, "RoomId", "RoomId");
+            return Page();
+        }
+
+
+        [BindProperty]
+        public HotelReservation HotelReservation { get; set; } = default!;
+
+        public HotelRoom HotelRoom { get; set; } = default!;
+        public string errormessage { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                errormessage = "feil i inputen";
+                return Page();
+            }
+
+            if(_context.HotelRooms.Any(h => HotelRoom.QualityOfRoom==h.QualityOfRoom && HotelRoom.NumberOfBeds == h.NumberOfBeds && HotelRoom.SizeOfRoom == h.SizeOfRoom && h.Occupied==false)) {
+                if (_context.HotelRooms.Any(h => h.HotelReservations.Any(r => r.StartDate <= HotelReservation.StartDate && r.EndDate >= HotelReservation.EndDate)))
+                {
+                    errormessage = "rommet er optatt i den tiden";
+                    return Page();
+                }
+                else
+                {
+                    int roomid = _context.HotelRooms.Where(h => HotelRoom.QualityOfRoom == h.QualityOfRoom && HotelRoom.NumberOfBeds == h.NumberOfBeds && HotelRoom.SizeOfRoom == h.SizeOfRoom && h.Occupied == false).ToList().First().RoomId;
+                    HotelReservation.RoomId = roomid;
+                    _context.HotelRooms.Where(h => HotelRoom.QualityOfRoom == h.QualityOfRoom && HotelRoom.NumberOfBeds == h.NumberOfBeds && HotelRoom.SizeOfRoom == h.SizeOfRoom && h.Occupied == false).ToList().First().HotelReservations.Add(HotelReservation);
+                    _context.HotelReservations.Add(HotelReservation);
+                    await _context.SaveChangesAsync();
+                    TempData["RoomId"] = HotelReservation.RoomId;
+                    errormessage = "";
+                    return RedirectToPage("./BestiltRom");
+                }
+            }
+            errormessage = "fant ikke rom med de kriteriene";
+            return Page();
+
+        }
+    }
+}
